0da0a944a6e140641868fee033c7f8005b3d2b34
m.delapasion <markcristiandelapasion@gmail.com>
2022-08-07 01:35:49 +0200

    New feature export

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,22 +1,33 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
-const request = require("request")
+const cors = require("cors")
+const investmentsService = require("./services/InvestmentsService")
+const fetch = require("node-fetch")
 
 const app = express()
 
+app.use(cors())
+
 app.use(bodyParser.json({limit: "10mb"}))
 
-app.get("/investments/:id", (req, res) => {
+app.get("/investments/:id", async (req, res) => {
   const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
+  const response = await fetch(`${config.investmentsServiceUrl}/investments/${id}`)
+  const data = await response.json()
+  res.send(data)
+})
+
+app.get("/investments/export/csv", async (req, res) => {
+
+  // requests can be refactor with promise.all to be async
+  const investments = await (await fetch(`${config.investmentsServiceUrl}/investments`)).json()
+  const companies = await (await fetch(`${config.companiesServiceUrl}/companies`)).json()
+  const csvFormatted = investmentsService.getCsvFormatted(investments, companies)
+
+  await fetch(`${config.investmentsServiceUrl}/investments/export`, {method: "POST", body: csvFormatted})
+
+  res.send(csvFormatted)
 })
 
 app.listen(config.port, (err) => {
